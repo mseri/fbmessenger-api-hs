8aa085c8b114499ec256c72d60263ba080797e3b
Alexander Krupenkin <mail@akru.me>
2017-02-28 11:36:57 +0300

    Ported to Servant 0.9

--- example-app/example.hs
+++ example-app/example.hs
@@ -1,32 +1,31 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
 
-import Control.Monad (when)
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Trans.Except (ExceptT)
+import           Control.Monad              (when)
+import           Control.Monad.IO.Class     (liftIO)
 import qualified Data.ByteString.Lazy.Char8 as C
 --import Data.Either (either)
-import Data.Foldable (traverse_)
-import Data.Maybe (fromMaybe)
-import Data.Proxy
-import qualified Data.Text as T
-import Network.HTTP.Client (newManager)
-import Network.HTTP.Client.TLS (tlsManagerSettings)
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Network.Wai.Logger (withStdoutLogger)
-import Servant
-import System.Environment
-import System.Exit (exitFailure)
-import System.IO
-import Text.Printf (printf)
-import Web.FBMessenger.API.Bot
+import           Data.Foldable              (traverse_)
+import           Data.Maybe                 (fromMaybe)
+import           Data.Proxy
+import qualified Data.Text                  as T
+import           Network.HTTP.Client        (newManager)
+import           Network.HTTP.Client.TLS    (tlsManagerSettings)
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Network.Wai.Logger         (withStdoutLogger)
+import           Servant
+import           System.Environment
+import           System.Exit                (exitFailure)
+import           System.IO
+import           Text.Printf                (printf)
+import           Web.FBMessenger.API.Bot
 
-type WebHookAPI = "webhook" :> 
-    QueryParam "hub.verify_token" String :> 
-    QueryParam "hub.challenge" String :> 
+type WebHookAPI = "webhook" :>
+    QueryParam "hub.verify_token" String :>
+    QueryParam "hub.challenge" String :>
     Get '[PlainText] String
   :<|> "webhook" :>
     ReqBody '[JSON] RemoteEventList :>
@@ -38,21 +37,19 @@ webHookAPI = Proxy
 -- TODO: use monad-logger to implement proper logging
 
 server :: String -> String -> Server WebHookAPI
-server verifyTokenStored pageTokenStored = 
+server verifyTokenStored pageTokenStored =
   webhook_verify
   :<|> webhook_message
   where
-    webhook_verify :: Maybe String -> Maybe String -> ExceptT ServantErr IO String
-    webhook_verify (Just verifyToken) (Just challenge) 
+    webhook_verify (Just verifyToken) (Just challenge)
       | verifyToken == verifyTokenStored = return challenge
     webhook_verify tk ch = let
       eB :: String
       eB = printf "[ERROR]: wrong validation request. Got (token, challenge) = (%s, %s)" (show tk) (show ch)
-      in do 
+      in do
         liftIO $ putStrLn eB
         throwError err500 { errBody = C.pack eB}
 
-    webhook_message :: RemoteEventList -> ExceptT ServantErr IO String
     webhook_message (RemoteEventList res) = do
       r <- traverse echoMessage (concatMap evt_messaging res)
       liftIO $ traverse_ (putStrLn . T.unpack) r
@@ -60,20 +57,19 @@ server verifyTokenStored pageTokenStored =
 
     token = Token $ T.pack pageTokenStored
 
-    echoMessage :: EventMessage -> ExceptT ServantErr IO T.Text
     echoMessage msg =
       case evtContent msg of
-        EmTextMessage _ _ text -> liftIO $ echoTextMessage text (evtSenderId msg) 
+        EmTextMessage _ _ text -> liftIO $ echoTextMessage text (evtSenderId msg)
         _                      -> return "[WARN]: this is just an example, no complex message is echoed."
-    
-    echoTextMessage text rcptId = do 
+
+    echoTextMessage text rcptId = do
       m <- newManager tlsManagerSettings
       -- create the recipient using the sender id
-      -- (we know that this is successful when only one argument is not Nothing) 
+      -- (we know that this is successful when only one argument is not Nothing)
       let (Just rcpt) = recipient (Just rcptId) Nothing
-      -- prepare the Send API request body 
+      -- prepare the Send API request body
       let messageReq = sendTextMessageRequest Nothing rcpt text
-      -- finally send the message request using the tls http connection manager  
+      -- finally send the message request using the tls http connection manager
       logRsp <- sendTextMessage (Just token) messageReq m
       -- restructure the response to get a decent log out of it
       -- e.g. one could add the response or the error in the log message
@@ -83,7 +79,7 @@ server verifyTokenStored pageTokenStored =
       return (T.pack logMsg)
 
 app :: String -> String -> Application
-app verifyToken pageToken = serve webHookAPI $ server verifyToken pageToken 
+app verifyToken pageToken = serve webHookAPI $ server verifyToken pageToken
 
 main :: IO ()
 main = do
--- src/Web/FBMessenger/API/Bot/SendAPI.hs
+++ src/Web/FBMessenger/API/Bot/SendAPI.hs
@@ -1,11 +1,11 @@
 -- |
--- Module      : Web.FBMessenger.API.Bot.Requests 
+-- Module      : Web.FBMessenger.API.Bot.Requests
 -- License     : BSD3
 -- Maintainer  : Marcello Seri <marcello.seri@gmail.com>
 -- Stability   : experimental
 -- Portability : unknown
 --
--- This module contains a 'servant' client for the 
+-- This module contains a 'servant' client for the
 -- <https://developers.facebook.com/docs/messenger-platform/ Messenger Platform API>
 -- and helpers useful to construct the appropriate requests and parse the responses.
 --
@@ -14,7 +14,7 @@
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE TypeOperators              #-}
 
-module Web.FBMessenger.API.Bot.SendAPI 
+module Web.FBMessenger.API.Bot.SendAPI
   ( -- * Functions
     getUserProfileInfo
   , removeWelcomeMessage
@@ -31,11 +31,10 @@ module Web.FBMessenger.API.Bot.SendAPI
   ) where
 
 
-import           Control.Monad.Trans.Except (runExceptT) -- ExceptT
 import           Data.Proxy
-import           Data.Text (Text)
-import qualified Data.Text as T
-import           Network.HTTP.Client (Manager)
+import           Data.Text                         (Text)
+import qualified Data.Text                         as T
+import           Network.HTTP.Client               (Manager)
 import           Servant.API
 import           Servant.Client
 import           Servant.Client.MultipartFormData
@@ -58,14 +57,14 @@ graphAPIBaseUrl = BaseUrl Https "graph.facebook.com" 443 "/v2.6"
 
 
 -- | Messenger Platform Send API
-type FBMessengerSendAPI = 
-         GraphAPIAccessToken :> "me" :> "messages" 
+type FBMessengerSendAPI =
+         GraphAPIAccessToken :> "me" :> "messages"
          :> ReqBody '[JSON] SendTextMessageRequest
          :> Post '[JSON] MessageResponse
     :<|> GraphAPIAccessToken :> "me" :> "messages"
          :> MultipartFormDataReqBody (UploadImageMessageRequest FileUpload)
          :> Post '[JSON] MessageResponse
-    :<|> GraphAPIAccessToken :> "me" :> "messages" 
+    :<|> GraphAPIAccessToken :> "me" :> "messages"
          :> ReqBody '[JSON] SendStructuredMessageRequest
          :> Post '[JSON] MessageResponse
     :<|> GraphAPIAccessToken :> "me" :> "subscribed_apps"
@@ -85,18 +84,18 @@ api :: Proxy FBMessengerSendAPI
 api = Proxy
 
 -- type ClientM = ExceptT ServantError IO
-sendTextMessage_       ::               Maybe Token -> SendTextMessageRequest -> Manager -> BaseUrl -> ClientM MessageResponse
-uploadImageMessage_    :: Maybe Token -> UploadImageMessageRequest FileUpload -> Manager -> BaseUrl -> ClientM MessageResponse
-sendStructuredMessage_ ::         Maybe Token -> SendStructuredMessageRequest -> Manager -> BaseUrl -> ClientM MessageResponse
-subscribedApps_        ::                                         Maybe Token -> Manager -> BaseUrl -> ClientM SubscriptionResponse
-welcomeMessage_        ::        Maybe Token -> Text -> WelcomeMessageRequest -> Manager -> BaseUrl -> ClientM WelcomeMessageResponse
-deleteWMessage_        ::        Maybe Token -> Text -> WelcomeMessageRequest -> Manager -> BaseUrl -> ClientM WelcomeMessageResponse
-userProfile_           ::                   Maybe Token -> Maybe Text -> Text -> Manager -> BaseUrl -> ClientM UserProfileResponse
+sendTextMessage_       ::               Maybe Token -> SendTextMessageRequest -> ClientM MessageResponse
+uploadImageMessage_    :: Maybe Token -> UploadImageMessageRequest FileUpload -> ClientM MessageResponse
+sendStructuredMessage_ ::         Maybe Token -> SendStructuredMessageRequest -> ClientM MessageResponse
+subscribedApps_        ::                                         Maybe Token -> ClientM SubscriptionResponse
+welcomeMessage_        ::        Maybe Token -> Text -> WelcomeMessageRequest -> ClientM WelcomeMessageResponse
+deleteWMessage_        ::        Maybe Token -> Text -> WelcomeMessageRequest -> ClientM WelcomeMessageResponse
+userProfile_           ::                   Maybe Token -> Maybe Text -> Text -> ClientM UserProfileResponse
 
 sendTextMessage_
   :<|> uploadImageMessage_
   :<|> sendStructuredMessage_
-  :<|> subscribedApps_ 
+  :<|> subscribedApps_
   :<|> welcomeMessage_
   :<|> deleteWMessage_
   :<|> userProfile_ = client api
@@ -104,46 +103,53 @@ sendTextMessage_
 
 -- | Send text messages. On success, minor informations on the sent message are returned.
 sendTextMessage :: Maybe Token -> SendTextMessageRequest -> Manager -> IO (Either ServantError MessageResponse)
-sendTextMessage = run graphAPIBaseUrl sendTextMessage_
+sendTextMessage token request manager =
+    runClientM (sendTextMessage_ token request) (ClientEnv manager graphAPIBaseUrl)
 
--- | Upload an image and send a structured messages containing it. 
+-- | Upload an image and send a structured messages containing it.
 --   On success, minor informations on the sent message are returned.
 uploadImageMessage :: Maybe Token -> UploadImageMessageRequest FileUpload -> Manager -> IO (Either ServantError MessageResponse)
-uploadImageMessage = run graphAPIBaseUrl uploadImageMessage_
+uploadImageMessage token request manager =
+    runClientM (uploadImageMessage_ token request) (ClientEnv manager graphAPIBaseUrl)
 
--- | Send a structured messages. This can be an image message (containing an image url) 
---   or any template message (generic, button, receipt).  
+-- | Send a structured messages. This can be an image message (containing an image url)
+--   or any template message (generic, button, receipt).
 --   On success, minor informations on the sent message are returned.
 sendStructuredMessage :: Maybe Token -> SendStructuredMessageRequest -> Manager -> IO (Either ServantError MessageResponse)
-sendStructuredMessage = run graphAPIBaseUrl sendStructuredMessage_
+sendStructuredMessage token request manager =
+    runClientM (sendStructuredMessage_ token request) (ClientEnv manager graphAPIBaseUrl)
 
 -- | Test if your bot's auth token is enabled. Requires no parameters.
---   Return a simple object containing a boolean value indicating if the 
+--   Return a simple object containing a boolean value indicating if the
 --   token is correctly registered.
 subscribedApps :: Maybe Token -> Manager -> IO (Either ServantError SubscriptionResponse)
-subscribedApps token manager = runExceptT $ subscribedApps_ token manager graphAPIBaseUrl
+subscribedApps token manager =
+    runClientM (subscribedApps_ token) (ClientEnv manager graphAPIBaseUrl)
 
--- | Set a welcome message, this can be an image message (containing an image url) 
+-- | Set a welcome message, this can be an image message (containing an image url)
 --   or any template message (generic, button, receipt).
---   In addition to the token and the message request, you need to provide the 
+--   In addition to the token and the message request, you need to provide the
 --   facebook page_id.
---   Return a simple object containing a string indicating if the welcome message 
+--   Return a simple object containing a string indicating if the welcome message
 --   is correctly registered.
 setWelcomeMessage :: Maybe Token -> Text -> WelcomeMessageRequest -> Manager -> IO (Either ServantError WelcomeMessageResponse)
-setWelcomeMessage token pageId message manager = runExceptT $ welcomeMessage_ token pageId message manager graphAPIBaseUrl
+setWelcomeMessage token pageId message manager =
+    runClientM (welcomeMessage_ token pageId message) (ClientEnv manager graphAPIBaseUrl)
 
--- | Remove the welcome message. In addition to the token, you need to provide 
+-- | Remove the welcome message. In addition to the token, you need to provide
 --   the facebook page_id.
---   Return a simple object containing a string indicating if the welcome 
+--   Return a simple object containing a string indicating if the welcome
 --   message is correctly removed.
 removeWelcomeMessage :: Maybe Token -> Text -> Manager -> IO (Either ServantError WelcomeMessageResponse)
-removeWelcomeMessage token pageId manager = runExceptT $ deleteWMessage_ token pageId welcomeDeleteMessage manager graphAPIBaseUrl
+removeWelcomeMessage token pageId manager =
+    runClientM (deleteWMessage_ token pageId welcomeDeleteMessage) (ClientEnv manager graphAPIBaseUrl)
 
 -- | Get the profile informations of a user. In addition to the token, you need
 --   to provide the user_id.
 --   Return a record containing the profile informations.
 getUserProfileInfo :: Maybe Token -> Text -> Manager -> IO (Either ServantError UserProfileResponse)
-getUserProfileInfo token userId manager = runExceptT $ userProfile_ token userProfileFields userId manager graphAPIBaseUrl
+getUserProfileInfo token userId manager =
+    runClientM (userProfile_ token userProfileFields userId) (ClientEnv manager graphAPIBaseUrl)
 
 
 -- Helpers (not exported)
@@ -153,6 +159,3 @@ userProfileFields = pure $ T.pack "first_name,last_name,profile_pic,locale,timez
 
 welcomeDeleteMessage :: WelcomeMessageRequest
 welcomeDeleteMessage = WelcomeEmptyMessage
-
-run :: BaseUrl -> (Maybe Token -> a -> Manager -> BaseUrl -> ClientM b) -> Maybe Token -> a -> Manager -> IO (Either ServantError b)
-run b e t r m = runExceptT $ e t r m b
